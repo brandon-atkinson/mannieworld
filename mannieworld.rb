acc76a418bc5c9305b25e8da904a66ef7c798f17
Brandon N. Atkinson <brandon.n.atkinson@gmail.com>
2013-12-07 22:09:50 -0500

    Added mannie to game

--- mannieworld.rb
+++ mannieworld.rb
@@ -4,12 +4,33 @@ class MannieWorld < Gosu::Window
    def initialize 
       super 1024, 768, false
       self.caption = "Mannie World"
+
+      @mannie = Mannie.new(self)
+      @mannie.warp(512, 384)
    end
 
    def update
+      if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then 
+         @mannie.turn_left
+      end
+      if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
+         @mannie.turn_right
+      end
+      if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
+         @mannie.accelerate
+      end
+
+      @mannie.move
    end
 
    def draw
+      @mannie.draw
+   end
+end
+
+def button_down(id)
+   if id == Gosu::KbEscape
+      close
    end
 end
 
